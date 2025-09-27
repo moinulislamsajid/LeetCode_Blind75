f7ff1a7b185e910d1e62487f7b6eb3752545d71f
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-09-27 22:06:19 +0600

    Add files via upload

---
+++ Blind_75/Day13/70. Climbing Stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+       if(n==0 || n==1)
+       {
+            return n;
+       }
+
+       int prev=1,curr=1;
+
+       for(int i=2; i<=n; i++)
+       {
+           int temp=curr;
+
+           curr= curr+prev;
+           prev=temp;
+       }
+
+       return curr;
+    }
+};
