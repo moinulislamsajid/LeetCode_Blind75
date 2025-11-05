6ddb89800933b546f30157b7ed587476ab657fa6
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-11-05 17:41:45 +0600

    Add files via upload

--- Blind_75/Day37/98. Validate Binary Search Tree.cpp
+++ Blind_75/Day37/98. Validate Binary Search Tree.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+public:
+
+bool isValidBST(TreeNode* root)
+{
+
+    solve(root,LONG_MIN,LONG_MAX);
+}
+
+private:
+
+{
+    bool solve(TreeNode* root,LONG mn,LONG,mx)
+    {
+
+        if(!root)
+        {
+            return true;
+        }
+        else if(!(root->val>mn) && (root->val<mx))
+        {
+            return false;
+        }
+
+        return solve(root->left,mn,root->val) && solve(root->right,root->val,mx);
+    }
+
+}
