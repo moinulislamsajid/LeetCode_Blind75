53a4b3fb74a7a566e6d506d4fe4d3458e23f2867
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-11-22 21:43:54 +0600

    Add files via upload

---
+++ Blind_75/Day_50/211. Design Add and Search Words Data Structure.cpp
@@ -0,0 +1,77 @@
+
+
+struct TrieNode
+{
+    bool isLastKey;
+
+    TrieNode* next[26];
+
+    TrieNode(): isLastKey(false)
+    {
+        memset(next,NULL,sizeof(next));
+    }
+
+};
+
+class  WordDictionary
+{
+public:
+
+    WordDictionary()
+    {
+        root = new TrieNode();
+    }
+
+    void addWord(string word)
+    {
+        TrieNode* node = root;
+
+        for(char ch : word)
+        {
+            if(!node->next[ch-'a']) node->next[ch-'a'] = new TrieNode();
+            node = node->next[ch-'a'];
+        }
+
+        node->isLastKey=true;
+    }
+
+    bool search(string word)
+    {
+        return solve(word,root);
+    }
+
+private:
+
+    TrieNode* root;
+    bool solve(string word,TrieNode* node)
+    {
+        for(int i=0; i<word.size(); i++)
+        {
+            char ch = word[i];
+
+            if(ch!='.')
+            {
+                if(!node->next[ch-'a']) return false;
+                node = node->next[ch-'a'];
+            }
+            else
+            {
+                bool flag = false;
+
+                int j=0;
+
+                for(;j<26;j++)
+                {
+                    if(node->next[j]) flag |= solve(word.substr(i+1),node->next[j]);
+                    if(flag) return true;
+
+                }
+
+                if(j==26) return false;
+            }
+        }
+
+        return node->isLastKey;
+    }
+
+};
